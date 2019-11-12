4df053f6983778fd32295dbbe72d0a421e391e71
edom18 <edo.m18@gmail.com>
2019-11-12 09:23:05 +0900

    feat: first commit.

---
+++ Assets/Scripts/Client.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Client : MonoBehaviour
+{
+    public class DummyPackImage : IPackImage
+    {
+        private int _width = 0;
+        private int _height = 0;
+
+        int IPackImage.Width => _width;
+        int IPackImage.Height => _height;
+
+        public DummyPackImage(int width, int height)
+        {
+            _width = width;
+            _height = height;
+        }
+    }
+
+    private Packer _packer = null;
+
+    private void Start()
+    {
+        IPackImage[] imgs = new IPackImage[]
+        {
+            new DummyPackImage(50, 130),
+            new DummyPackImage(150, 10),
+            new DummyPackImage(250, 300),
+            new DummyPackImage(350, 80),
+            new DummyPackImage(150, 30),
+        };
+
+        _packer = new Packer(1024);
+        _packer.SetImages(imgs);
+    }
+}
---
+++ Assets/Scripts/Packer.cs
@@ -0,0 +1,113 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface IPackImage
+{
+    int Width { get; }
+    int Height { get; }
+}
+
+public class Node
+{
+    public Node[] Child = new Node[2];
+    public Rect Rectangle = default;
+    public int ImageID = -1;
+
+    private bool _isLeafNode = true;
+
+    private bool CheckFitInRect(IPackImage image)
+    {
+        bool isInRect = (image.Width <= Rectangle.width) &&
+                        (image.Height <= Rectangle.height);
+        return isInRect;
+    }
+
+    private bool CheckFitPerfectly(IPackImage image)
+    {
+        bool isSameBoth = (image.Width == Rectangle.width) &&
+                          (image.Height == Rectangle.height);
+        return isSameBoth;
+    }
+
+    public Node Insert(IPackImage image)
+    {
+        if (!_isLeafNode)
+        {
+            Node newNode = Child[0].Insert(image);
+            if (newNode != null)
+            {
+                return newNode;
+            }
+
+            return Child[1].Insert(image);
+        }
+        else
+        {
+            if (ImageID != -1)
+            {
+                return null;
+            }
+
+            if (!CheckFitInRect(image))
+            {
+                return null;
+            }
+
+            if (CheckFitPerfectly(image))
+            {
+                return this;
+            }
+
+            _isLeafNode = false;
+
+            Child[0] = new Node();
+            Child[1] = new Node();
+
+            float dw = Rectangle.width - image.Width;
+            float dh = Rectangle.height - image.Height;
+
+            if (dw > dh)
+            {
+                // child[0]->rect = Rectangle(rc.left, rc.top, rc.left + width - 1, rc.bottom)
+                // child[1]->rect = Rectangle(rc.left + width, rc.top, rc.right, rc.bottom)
+                Child[0].Rectangle = new Rect(Rectangle.x, Rectangle.y, image.Width - 1, image.Height);
+                Child[1].Rectangle = new Rect(Rectangle.x + image.Width, Rectangle.y, Rectangle.width - image.Width, image.Height);
+            }
+            else
+            {
+                // child[0]->rect = Rectangle(rc.left, rc.top, rc.right, rc.top + height - 1)
+                // child[1]->rect = Rectangle(rc.left, rc.top + height, rc.right, rc.bottom)
+                Child[0].Rectangle = new Rect(Rectangle.x, Rectangle.y, Rectangle.width, image.Height - 1);
+                Child[1].Rectangle = new Rect(Rectangle.x, Rectangle.y + image.Height, Rectangle.width, Rectangle.height - image.Height);
+            }
+
+            return Child[0];
+        }
+    }
+
+    public void SetImage(int imageID)
+    {
+        ImageID = imageID;
+        _isLeafNode = true;
+    }
+}
+
+public class Packer
+{
+    private Node _rootNode = null;
+
+    public Packer(int size)
+    {
+        _rootNode = new Node();
+        _rootNode.Rectangle = new Rect(0, 0, size, size);
+    }
+
+    public void SetImages(IPackImage[] images)
+    {
+        foreach (var img in images)
+        {
+            Node node = _rootNode.Insert(img);
+        }
+    }
+}
