99eef8677f1b2725e4da79191bdbe7403e2fd4e1
edom18 <edo.m18@gmail.com>
2019-11-15 10:21:35 +0900

    feat: Moved DummyPackImage class to a file.

--- Assets/Scripts/PackClient.cs
+++ Assets/Scripts/PackClient.cs
@@ -8,44 +8,6 @@ using SimpleTexturePacker.Infrastructure;
 
 public class PackClient : MonoBehaviour
 {
-    public class DummyPackImage : IPackImage
-    {
-        private int _width = 0;
-        private int _height = 0;
-        private Texture _texture = null;
-
-        private Color _color = Color.white;
-
-        int IPackImage.Width => _width;
-        int IPackImage.Height => _height;
-        Texture IPackImage.Texture => _texture;
-
-        public DummyPackImage(int width, int height, Color color)
-        {
-            _width = width;
-            _height = height;
-            _color = color;
-
-            GenerateDummyTexture();
-        }
-
-        private void GenerateDummyTexture()
-        {
-            Color[] colors = new Color[_width * _height];
-
-            for (int i = 0; i < colors.Length; i++)
-            {
-                colors[i] = _color;
-            }
-
-            Texture2D tex = new Texture2D(_width, _height);
-            tex.SetPixels(colors);
-            tex.Apply();
-
-            _texture = tex;
-        }
-    }
-
     [SerializeField] private Material _material = null;
 
     private PackService _packerService = null;
