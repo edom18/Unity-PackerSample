ee82a89762cf353729ca8aa29af0b1ce8e64e788
edom18 <edo.m18@gmail.com>
2019-11-15 10:21:53 +0900

    feat: Added a PackImage implement.

---
+++ Assets/Scripts/PackImage.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using SimpleTexturePacker.Domain;
+
+namespace SimpleTexturePacker.Infrastructure
+{
+    public class PackImage : IPackImage
+    {
+        private Texture _texture = null;
+
+        int IPackImage.Width => _texture.width;
+        int IPackImage.Height => _texture.height;
+        Texture IPackImage.Texture => _texture;
+
+        public PackImage(Texture texture)
+        {
+            _texture = texture;
+        }
+    }
+}
